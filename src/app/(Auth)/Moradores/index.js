d3ef7c88aa5cb35f43a404c32a85e6baf13e33aa
Jonathan-Albuquerque-dev <jonathan.albuquerque.dev@gmail.com>
2024-10-27 03:42:23 -0300

    criado tela de moradores

--- src/app/(Auth)/Moradores/index.js
+++ src/app/(Auth)/Moradores/index.js
@@ -1,9 +1,62 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Residentes } from "../../../database/moradores.js";
 
 export default function Moradores() {
   return (
     <View>
-      <Text>Moradores</Text>
+      <FlatList
+        data={Residentes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <MoradoresList itens={item} />}
+      />
     </View>
   );
 }
+
+function MoradoresList({ itens }) {
+  return (
+    <TouchableOpacity style={style.container}>
+      <Image source={require("../../../assets/person.png")} style={style.img} />
+      <View style={style.containerNome}>
+        <Text>{itens.nome}</Text>
+        <View style={style.containerBlocoAp}>
+          <Text>bloco: {itens.bloco}</Text>
+          <Text>apt: {itens.apt}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#23C0DB",
+    gap: 20,
+    borderWidth: 1,
+    marginHorizontal: 10,
+    marginBottom: 5,
+    marginTop: 5,
+    borderRadius: 7,
+    padding: 10,
+    flexDirection: "row",
+  },
+  containerNome: {
+    gap: 10,
+  },
+  containerBlocoAp: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  img: {
+    width: 50,
+    height: 50,
+  },
+});
